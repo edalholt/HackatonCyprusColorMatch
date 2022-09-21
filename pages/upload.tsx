056ffb6113b019127ebaacde1a60edcecc459229
Thea Kvinnegard <thea.kvinnegard@gmail.com>
2022-09-21 12:08:28 +0200

    fix header and frontpage

---
+++ pages/upload.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+import React from 'react'
+import Header from '../components/header'
+import { faDove } from '@fortawesome/free-solid-svg-icons'
+
+const Home: NextPage = () => {
+    const router = useRouter(); 
+    
+  return (
+    <div>
+        <Header />
+        <div style={{backgroundColor: '#284b63', height: '100vh', display: 'flex', alignItems:'center', justifyContent:'center'}}>  
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
